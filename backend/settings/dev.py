e4805ff089267efcd0402ca3191f9d07143d6dba
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-03-28 14:56:37 -0400

    Update project-makefile

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(!ty3-d4(fz$rr0em$@(qur^vbg%i(r@+(cky=wh-@-t^h&kpp"
+SECRET_KEY = "django-insecure-t1ofa2y21%zyr%+=o+^j5depna_0-w1qewv8m_#xe5**68he0("
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
@@ -16,7 +16,9 @@ try:
     from .local import *
 except ImportError:
     pass
-INSTALLED_APPS.append('debug_toolbar')
-MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
-MIDDLEWARE.append('hijack.middleware.HijackUserMiddleware')
-INTERNAL_IPS = ["127.0.0.1",]
+INSTALLED_APPS.append("debug_toolbar")
+MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+MIDDLEWARE.append("hijack.middleware.HijackUserMiddleware")
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- modelformtest/admin.py
+++ modelformtest/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-
 from .models import TestModel
 
-admin.site.register(TestModel, TestModelAdmin)
+
+@admin.register(TestModel)
+class TestModelAdmin(admin.ModelAdmin):
+    pass
