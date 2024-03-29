d95f8c215b618c9b122a3580178abeb240001b67
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-03-29 13:25:12 -0400

    Update project-makefile

--- modelformtest/views.py
+++ modelformtest/views.py
@@ -1,28 +1,31 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from .models import TestModel
 from .forms import TestModelForm
 
+
 class TestModelListView(ListView):
     model = TestModel
-    template_name = 'test_model_list.html'  # Update with your template name
-    context_object_name = 'test_models'
+    template_name = "test_model_list.html"
+    context_object_name = "test_models"
+
 
 class TestModelCreateView(CreateView):
     model = TestModel
     form_class = TestModelForm
-    template_name = 'test_model_form.html'  # Update with your template name
+    template_name = "test_model_form.html"
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
+
 class TestModelUpdateView(UpdateView):
     model = TestModel
     form_class = TestModelForm
-    template_name = 'test_model_form.html'  # Update with your template name
+    template_name = "test_model_form.html"
+
 
 class TestModelDetailView(DetailView):
     model = TestModel
-    template_name = 'test_model_detail.html'  # Update with your template name
-    context_object_name = 'test_model'
+    template_name = "test_model_detail.html"
+    context_object_name = "test_model"
